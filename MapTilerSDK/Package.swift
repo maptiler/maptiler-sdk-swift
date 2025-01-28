5b43952451163f967a9207d6d994513e4911df88
sasaprodribaba <sasa.prodribaba@maptiler.com>
2025-01-28 11:19:19 +0100

    RD-588: Set up Swift Package workspace

---
+++ MapTilerSDK/Package.swift
@@ -0,0 +1,25 @@
+// swift-tools-version: 6.0
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "MapTilerSDK",
+    platforms: [
+        // Minimum Supported Version
+        .iOS(.v15)
+    ],
+    products: [
+        .library(
+            name: "MapTilerSDK",
+            targets: ["MapTilerSDK"]),
+    ],
+    targets: [
+        .target(
+            name: "MapTilerSDK"),
+        .testTarget(
+            name: "MapTilerSDKTests",
+            dependencies: ["MapTilerSDK"]
+        ),
+    ]
+)

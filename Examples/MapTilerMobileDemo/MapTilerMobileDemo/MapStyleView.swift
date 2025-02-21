b7c172bcfe91b84f26724e7314c9d722fcc9718b
sasaprodribaba <sasa.prodribaba@maptiler.com>
2025-02-21 10:03:33 +0100

    RD-617: Update demo for recognizers

--- Examples/MapTilerMobileDemo/MapTilerMobileDemo/MapStyleView.swift
+++ Examples/MapTilerMobileDemo/MapTilerMobileDemo/MapStyleView.swift
@@ -10,9 +10,7 @@ struct MapStyleView: View {
     enum Constants {
         static let emptyString = ""
         static let defaultOpacity: CGFloat = 0.7
-        static let maxHeight: CGFloat = 100
-        static let widthOffset: CGFloat = 0.85
-        static let heightOffset: CGFloat = 0.9
+        static let minHeight: CGFloat = 40
     }
 
     @State private var referenceStyle: MTMapReferenceStyle = .streets
@@ -23,59 +21,49 @@ struct MapStyleView: View {
     }
 
     var body: some View {
-        GeometryReader { geometry in
-            VStack {
-                MTMapViewContainer(options: MTMapOptions(attributionControlIsVisible: false))
-                    .referenceStyle(referenceStyle)
-                    .styleVariant(styleVariant)
-                    .frame(maxHeight: geometry.size.height * Constants.heightOffset)
-                    .ignoresSafeArea()
+        VStack {
+            MTMapViewContainer(options: MTMapOptions(attributionControlIsVisible: false))
+                .referenceStyle(referenceStyle)
+                .styleVariant(styleVariant)
 
-                HStack(alignment: .top) {
-                    VStack {
-                        Picker(Constants.emptyString, selection: $referenceStyle) {
-                            ForEach(MTMapReferenceStyle.allCases) { style in
-                                Text(style.rawValue.capitalized)
-                                    .accentColor(.white)
-                                    .foregroundStyle(.white)
-                                    .tint(.white)
-                                    .opacity(Constants.defaultOpacity)
-                                    .tag(style)
-                            }
-                        }
-                        .pickerStyle(.menu)
-                        .tint(.white)
-                        .onChange(of: referenceStyle) { _ in
-                            styleVariant = nil
-
-                            if let variant = selectedStyleVariants?.first {
-                                styleVariant = variant
-                            }
-                        }
-                        .frame(maxHeight: .infinity, alignment: .top)
-                    }
-
-                    VStack {
-                        Picker(Constants.emptyString, selection: $styleVariant) {
-                            if let selectedStyleVariants = selectedStyleVariants {
-                                ForEach(selectedStyleVariants) { style in
-                                    Text(style.rawValue.capitalized)
-                                        .accentColor(.white)
-                                        .foregroundStyle(.white)
-                                        .tint(.white)
-                                        .opacity(Constants.defaultOpacity)
-                                        .tag(style)
-                                }
-                            }
-                        }
-                        .pickerStyle(.menu)
-                        .tint(.white)
-                        .disabled(selectedStyleVariants?.isEmpty ?? false)
-                        .frame(maxHeight: .infinity, alignment: .top)
+            HStack(alignment: .top) {
+                Picker(Constants.emptyString, selection: $referenceStyle) {
+                    ForEach(MTMapReferenceStyle.allCases) { style in
+                        Text(style.rawValue.capitalized)
+                            .accentColor(.white)
+                            .foregroundStyle(.white)
+                            .tint(.white)
+                            .opacity(Constants.defaultOpacity)
+                            .tag(style)
                     }
                 }
-                .frame(maxWidth: geometry.size.width * Constants.widthOffset, maxHeight: Constants.maxHeight)
-                .ignoresSafeArea()
+                .pickerStyle(.menu)
+                .tint(.white)
+                .onChange(of: referenceStyle) { _ in
+                    styleVariant = nil
+
+                    if let variant = selectedStyleVariants?.first {
+                        styleVariant = variant
+                    }
+                }
+                .frame(height: Constants.minHeight, alignment: .top)
+
+                Picker(Constants.emptyString, selection: $styleVariant) {
+                    if let selectedStyleVariants = selectedStyleVariants {
+                        ForEach(selectedStyleVariants) { style in
+                            Text(style.rawValue.capitalized)
+                                .accentColor(.white)
+                                .foregroundStyle(.white)
+                                .tint(.white)
+                                .opacity(Constants.defaultOpacity)
+                                .tag(style)
+                        }
+                    }
+                }
+                .pickerStyle(.menu)
+                .tint(.white)
+                .disabled(selectedStyleVariants?.isEmpty ?? false)
+                .frame(height: Constants.minHeight, alignment: .top)
             }
         }
     }

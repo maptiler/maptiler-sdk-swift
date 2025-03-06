918f18f69bc751ce464d15285e70a43f897384b1
sasaprodribaba <sasa.prodribaba@maptiler.com>
2025-03-06 12:45:36 +0100

    RD-714: Add vector tiles source

---
+++ MapTilerSDK/Sources/MapTilerSDK/Commands/Style/SetUrlToSource.swift
@@ -0,0 +1,13 @@
+//
+//  SetUrlToSource.swift
+//  MapTilerSDK
+//
+import Foundation
+package struct SetUrlToSource: MTCommand {
+    var url: URL
+    var source: MTSource
+
+    package func toJS() -> JSString {
+        return "\(MTBridge.mapObject).getSource('\(source.identifier)').setUrl('\(url)');"
+    }
+}
---
+++ MapTilerSDK/Sources/MapTilerSDK/Map/Style/MTTileScheme.swift
@@ -0,0 +1,13 @@
+//
+//  MTTileScheme.swift
+//  MapTilerSDK
+//
+
+/// A scheme used for tiles.
+public enum MTTileScheme: String {
+    /// XYZ Format.
+    case xyz
+
+    /// TMS Format.
+    case tms
+}

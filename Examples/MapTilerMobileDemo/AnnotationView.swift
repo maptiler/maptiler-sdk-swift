c9aafb62f554d8a7723991fedcb563e5561e1472
sasaprodribaba <sasa.prodribaba@maptiler.com>
2025-04-14 12:10:19 +0200

    RD-817: Add Demo annotation view

---
+++ Examples/MapTilerMobileDemo/AnnotationView.swift
@@ -0,0 +1,35 @@
+//
+//  AnnotationView.swift
+//  MapTilerMobileDemo
+//
+
+import UIKit
+import MapTilerSDK
+import CoreLocation
+
+class AnnotationView: MTCustomAnnotationView, @unchecked Sendable {
+    @IBOutlet weak var contentView: UIView!
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+
+        commonInit()
+    }
+
+    override init(frame: CGRect, coordinates: CLLocationCoordinate2D) {
+        super.init(frame: frame, coordinates: coordinates)
+
+        commonInit()
+    }
+
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+
+        commonInit()
+    }
+
+    func commonInit() {
+        Bundle.main.loadNibNamed(String(describing: type(of: self)), owner: self)
+        addSubview(contentView)
+    }
+}

f87958b0626a1d3c786b2f4061a029b8ae04bf5f
sasaprodribaba <sasa.prodribaba@maptiler.com>
2026-03-15 16:59:36 +0100

    RD-1618: remove sh

--- update_tests.sh
+++
@@ -1,8 +0,0 @@
-sed -i '' -e '/@Test func setSpriteCommand_shouldGenerateExpectedJS/i\
-    @Test func removeSpriteCommand_shouldGenerateExpectedJS() async throws {\
-        let command = RemoveSprite(id: "test-sprite")\
-        let expectedJS = "\\(MTBridge.mapObject).removeSprite(\\"test-sprite\\");"\
-\
-        #expect(command.toJS() == expectedJS)\
-    }\
-' Tests/MapTilerSDKTests/MTStyleTests.swift

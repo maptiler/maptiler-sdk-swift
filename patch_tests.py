23b8e4d1d7ee5b6022aeed0b9633f48701e25f60
sasaprodribaba <sasa.prodribaba@maptiler.com>
2026-03-17 11:00:04 +0100

    RD-1636: remove patch

--- patch_tests.py
+++
@@ -1,8 +0,0 @@
-import re
-with open('Tests/MapTilerSDKTests/MTStyleTests.swift', 'r') as f:
-    content = f.read()
-
-content = content.replace('#expect(js.contains("\\(MTBridge.mapObject).style.addImage(\\"---!@#\\",")))', '#expect(js.contains("\\(MTBridge.mapObject).style.addImage(\\"---!@#\\","))')
-
-with open('Tests/MapTilerSDKTests/MTStyleTests.swift', 'w') as f:
-    f.write(content)
--- test.swift
+++
@@ -1,6 +0,0 @@
-import Foundation
-
-let name = "---!@#"
-let data = try! JSONEncoder().encode(name)
-let string = String(data: data, encoding: .utf8)!
-print(string)

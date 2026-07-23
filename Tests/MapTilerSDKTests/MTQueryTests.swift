593583f1260eec35b7b3375ed3baccff7ab1755f
sasaprodribaba <sasa.prodribaba@maptiler.com>
2026-07-23 16:21:57 +0200

    Fix test

--- Tests/MapTilerSDKTests/MTQueryTests.swift
+++ Tests/MapTilerSDKTests/MTQueryTests.swift
@@ -15,7 +15,7 @@ struct MTQueryTests {
         let command = QueryRenderedFeatures(point: point, layers: ["layer1"], filter: "[\"==\", \"id\", 1]")
         let jsString = command.toJS()
         
-        #expect(jsString.contains("map.queryRenderedFeatures({x: 100.0, y: 200.0}"))
+        #expect(jsString.contains("map.queryRenderedFeatures([100.000000, 200.000000]"))
         #expect(jsString.contains("\"layers\":[\"layer1\"]"))
         #expect(jsString.contains("\"filter\":\"[\\\"==\\\", \\\"id\\\", 1]\""))
         #expect(jsString.contains("JSON.stringify"))
